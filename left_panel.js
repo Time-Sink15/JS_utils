271347d869064aaa37847df6725810d29ee1bb9c
Time-Sink15 <156330340+Time-Sink15@users.noreply.github.com>
2025-11-19 17:33:18 -0500

    Update left_panel.js

--- left_panel.js
+++ left_panel.js
@@ -102,7 +102,7 @@ const tabRow = document.createElement('div');
 tabRow.className = 'tm-tabrow';
 
 const tabSeparator = document.createElement('div');
-tabSeparator.className = 'tm-tab-separator';
+tabSeparator.className = 'tab-separator';
 
 const body = document.createElement('div');
 body.className = 'tm-body';
